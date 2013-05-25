a72a259828894c99697464c7052d8a2f38a5bd02
李林 <ww1095@163.com>
2013-05-25 21:16:26 +0800

    修改tab和navi合并demo

--- UITabBarController/4.UITabBarAndUINavigationDemo/1.UITabBarControllerDemo/AppDelegate.m
+++ UITabBarController/4.UITabBarAndUINavigationDemo/1.UITabBarControllerDemo/AppDelegate.m
@@ -27,31 +27,20 @@
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     
-    /*
-     * 1. 创建若干个子视图控制器（它们是并列的关系）
-     *  1.1 创建UITabBarItem实例，赋值给相应的子视图控制器（有可能是导航控制器）
-     * 2. 创建一个数组，将已创建的子视图控制器，添加到数组中
-     * 3. 创建UITabBarController实例
-     * 4. tabBarController.viewControllers = viewControllers;
-     * 5. 添加到window的rootViewController中
-     */
-    
     // 首页
     HomeViewController *vc1 = [[HomeViewController alloc] init];
     UINavigationController *homeNav = [[UINavigationController alloc] initWithRootViewController:vc1];
     [vc1 release];
+    
     UITabBarItem *homeItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemFavorites tag:1];
     vc1.tabBarItem = homeItem;
     [homeItem release];
-    /*
-    NSLog(@"vc1 barButtonItem : %p", vc1.navigationItem.leftBarButtonItem);
-    NSLog(@"vc1 tabBarItem : %p", vc1.tabBarItem);
-     */
     
     // 消息页
     MesssageViewController *vc2 = [[MesssageViewController alloc] init];
     UINavigationController *msgNav = [[UINavigationController alloc] initWithRootViewController:vc2];
     [vc2 release];
+    
     UITabBarItem *msgItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemContacts tag:2];
     vc2.tabBarItem = msgItem;
     [msgItem release];
@@ -60,6 +49,7 @@
     SearchViewController *vc3 = [[SearchViewController alloc] init];
     UINavigationController *searchNav = [[UINavigationController alloc] initWithRootViewController:vc3];
     [vc3 release];
+    
     UITabBarItem *searchItem = [[UITabBarItem alloc] initWithTitle:@"搜索" image:[UIImage imageNamed:@"alarm"] tag:3];
     vc3.tabBarItem = searchItem;
     [searchItem release];
@@ -68,6 +58,7 @@
     SettingViewController *vc4 = [[SettingViewController alloc] init];
     UINavigationController *setNav = [[UINavigationController alloc] initWithRootViewController:vc4];
     [vc4 release];
+    
     UITabBarItem *setItem = [[UITabBarItem alloc] initWithTitle:@"草莓" image:[UIImage imageNamed:@"Strawberry"] tag:4];
     vc4.tabBarItem = setItem;
     [setItem release];
@@ -76,6 +67,7 @@
     OtherViewController *vc5 = [[OtherViewController alloc] init];
     UINavigationController *otherNav1 = [[UINavigationController alloc] initWithRootViewController:vc5];
     [vc5 release];
+    
     UITabBarItem *otherItem1 = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemHistory tag:5];
     vc5.tabBarItem = otherItem1;
     [otherItem1 release];
@@ -84,6 +76,7 @@
     OtherViewController *vc6 = [[OtherViewController alloc] init];
     UINavigationController *otherNav2 = [[UINavigationController alloc] initWithRootViewController:vc6];
     [vc6 release];
+    
     UITabBarItem *otherItem2 = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemDownloads tag:6];
     vc6.tabBarItem = otherItem2;
     [otherItem2 release];
@@ -92,6 +85,7 @@
     OtherViewController *vc7 = [[OtherViewController alloc] init];
     UINavigationController *otherNav3 = [[UINavigationController alloc] initWithRootViewController:vc7];
     [vc7 release];
+    
     UITabBarItem *otherItem3 = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemRecents tag:7];
     vc7.tabBarItem = otherItem3;
     [otherItem3 release];
@@ -105,12 +99,8 @@
     
     // 创建UITabBarController实例
     UITabBarController *tabBarController = [[UITabBarController alloc] init];
-    // tabBarController.viewControllers = viewControllers;
     tabBarController.delegate = self; // 设置代理
     [tabBarController setViewControllers:viewControllers animated:YES];
-    
-    // 第二种集成方式，错误的集成方式
-//    UINavigationController *navigation = [[UINavigationController alloc] initWithRootViewController:tabBarController];
     
     self.window.rootViewController = tabBarController;
     [tabBarController release];
@@ -122,15 +112,16 @@
 - (BOOL)tabBarController:(UITabBarController *)tabBarController
 shouldSelectViewController:(UIViewController *)viewController
 {
-//    NSLog(@"shouldSelectViewController");
     return YES; // flag
-} // 视图将要切换时调用， viewController为将要显示的控制器
+}
+// 视图将要切换时调用， viewController为将要显示的控制器
 
 - (void)tabBarController:(UITabBarController *)tabBarController
  didSelectViewController:(UIViewController *)viewController
 {
-//    NSLog(@"didSelectViewController : %@", viewController);
-} // 视图已经切换后调用， viewController 是已经显示的控制器
+
+}
+// 视图已经切换后调用， viewController 是已经显示的控制器
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
@@ -140,7 +131,7 @@ shouldSelectViewController:(UIViewController *)viewController
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
